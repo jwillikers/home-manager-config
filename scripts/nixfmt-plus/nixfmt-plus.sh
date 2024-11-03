71ca9f27cf40e6e64547afc8d50cea08381d94c3
Jordan Williams <jordan@jwillikers.com>
2024-11-03 06:58:01 -0600

    Run nixfmt-plus against files passed as arguments

--- scripts/nixfmt-plus/nixfmt-plus.sh
+++ scripts/nixfmt-plus/nixfmt-plus.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+set -eou pipefail
 
 if [ $# -eq 0 ]; then
   deadnix --edit
   statix fix
   nixfmt --verify .
 else
-  deadnix --edit "$1"
-  statix fix "$1"
-  nixfmt --verify "$1"
+  deadnix --edit "$@"
+  for target in "$@"; do
+    statix fix -- "$target"
+  done
+  nixfmt --verify "$@"
 fi
