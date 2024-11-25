47d9c1d362c5a1f0a5ca214f244c217c2ef50bde
Jordan Williams <jordan@jwillikers.com>
2024-11-25 13:05:16 -0600

    Revert "Don't even bother with the variable"
    This reverts commit ade4e626ca4d2fe8614cb647c4a4746e94af43a5.

--- _mixins/desktop/kde/default.nix
+++ _mixins/desktop/kde/default.nix
@@ -7,7 +7,7 @@
 {
   home = {
     file = {
-      ".gnupg/gpg-agent.conf".text = ''
+      "${config.home.homeDirectory}/.gnupg/gpg-agent.conf".text = ''
         pinentry-program ${lib.getBin pkgs.kwalletcli}/bin/pinentry-kwallet
       '';
       # Go to System Settings > KDE Wallet and enable Use KWallet for the Secret Service interface. ??
