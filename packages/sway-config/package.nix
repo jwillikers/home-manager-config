32482b6d59bf1130111118e914847cdc2e30f20e
Jordan Williams <jordan@jwillikers.com>
2025-01-02 08:35:46 -0600

    Add support for clipse

--- packages/sway-config/package.nix
+++ packages/sway-config/package.nix
@@ -1,4 +1,5 @@
 {
+  clipse,
   fetchFromGitea,
   glib,
   lib,
@@ -8,14 +9,14 @@
 
 stdenvNoCC.mkDerivation {
   pname = "sway-config";
-  version = "0-unstable-2024-10-30";
+  version = "0-unstable-2025-01-02";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "jwillikers";
     repo = "sway-config";
-    rev = "bbf3a1d56b9c7505c9c5000dcb24e7d9c99a3b36";
-    hash = "sha256-NyWsax2+lZkbHrmxW8czARfoSo3HUlMwNIntYLQ9S68=";
+    rev = "52f6f11250a0b483ba0995716b35cecdca3a5e6c";
+    hash = "sha256-tnaUKpYKhuIWpvMClvgnL7TFclX27OM6pSOz77s+iR8=";
   };
 
   installPhase = ''
@@ -25,6 +26,8 @@ stdenvNoCC.mkDerivation {
   '';
 
   postFixup = ''
+    substituteInPlace $out/etc/sway/config.d/99-clipse.conf \
+      --replace-fail "exec clipse" "exec ${lib.getExe clipse}"
     substituteInPlace $out/etc/sway/config.d/99-swayaudioidleinhibit.conf \
       --replace-fail "exec sway-audio-idle-inhibit" "exec ${lib.getExe sway-audio-idle-inhibit}"
     substituteInPlace $out/etc/sway/config.d/99-gtk-dark-theme.conf \
