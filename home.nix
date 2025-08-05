8e62af2c0c3fe024104a16fe01736b3c3c495c15
Jordan Williams <jordan@jwillikers.com>
2025-08-05 18:05:28 -0500

    Try KillMode = "mixed"

--- home.nix
+++ home.nix
@@ -764,6 +764,7 @@ in
           ExecStartPre = "${lib.getBin pkgs.coreutils}/bin/sleep 1";
           # Can't use Nix's flatpak command with electron apps for reasons.
           ExecStart = "${lib.getExe pkgs.stretchly}";
+          KillMode = "mixed";
           Restart = "on-failure";
           RestartSec = 10;
         };
