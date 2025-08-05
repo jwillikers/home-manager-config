2f4c0175bac0214cf0fe568f87613e606b477ec0
Jordan Williams <jordan@jwillikers.com>
2025-08-05 18:37:15 -0500

    Try type=forking

--- home.nix
+++ home.nix
@@ -760,7 +760,7 @@ in
         };
 
         Service = {
-          Type = "simple";
+          Type = "forking";
           ExecStartPre = "${lib.getBin pkgs.coreutils}/bin/sleep 1";
           # Can't use Nix's flatpak command with electron apps for reasons.
           ExecStart = "-${lib.getExe pkgs.stretchly}";
