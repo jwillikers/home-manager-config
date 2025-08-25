37ef671d5e9da9a8a253859e237e185cd1d0ce9f
Jordan Williams <jordan@jwillikers.com>
2025-08-24 23:09:33 -0500

    Try again

--- _mixins/features/gaming/lutris/default.nix
+++ _mixins/features/gaming/lutris/default.nix
@@ -22,30 +22,31 @@ lib.mkIf (lib.elem hostname installOn) {
         onChange = ''cat ${config.xdg.dataHome}/lutris/system_source.yml > ${config.xdg.dataHome}/lutris/system.yml'';
       };
     };
-    packages = with pkgs; [
-      (config.lib.nixGL.wrap lutris)
-    ];
+    # packages = with pkgs; [
+    #   (config.lib.nixGL.wrap lutris)
+    # ];
   };
-  # todo Installing Lutris this way breaks everything.
-  # programs.lutris = {
-  #   enable = true;
-  #   extraPackages = with pkgs; [
-  #     mangohud
-  #     winetricks
-  #     config.lib.nixGL.wrap gamescope
-  #     gamemode
-  #     umu-launcher
-  #   ];
-  #   protonPackages = with pkgs; [ proton-ge-bin ];
-  #   package = config.lib.nixGL.wrap pkgs.lutris;
-  #   runners = {
-  #     scummvm.package = config.lib.nixGL.wrap pkgs.scummvm;
-  #   };
-  #   steamPackage =
-  #     if hostname == "steamdeck" then
-  #       (pkgs.runCommandLocal "empty" { } "mkdir $out")
-  #     else
-  #       (config.lib.nixGL.wrap pkgs.steam);
-  #   winePackages = with pkgs; [ wineWow64Packages.full ];
-  # };
+  # todo Installing Lutris this way breaks everything on the Steam Deck.
+  # It seems to work okay otherwise, but the Ludusavi configuration isn't kept.
+  programs.lutris = {
+    enable = true;
+    extraPackages = with pkgs; [
+      mangohud
+      winetricks
+      (config.lib.nixGL.wrap gamescope)
+      gamemode
+      umu-launcher
+    ];
+    protonPackages = with pkgs; [ proton-ge-bin ];
+    package = config.lib.nixGL.wrap pkgs.lutris;
+    runners = {
+      scummvm.package = config.lib.nixGL.wrap pkgs.scummvm;
+    };
+    steamPackage =
+      if hostname == "steamdeck" then
+        (pkgs.runCommandLocal "empty" { } "mkdir $out")
+      else
+        (config.lib.nixGL.wrap pkgs.steam);
+    winePackages = with pkgs; [ wineWow64Packages.full ];
+  };
 }
