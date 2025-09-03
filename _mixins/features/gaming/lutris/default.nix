56e946396c49556730cf9377d02568277f946774
Jordan Williams <jordan@jwillikers.com>
2025-09-03 12:14:01 -0500

    Fix path of Lutris system config

--- _mixins/features/gaming/lutris/default.nix
+++ _mixins/features/gaming/lutris/default.nix
@@ -17,9 +17,9 @@ lib.mkIf (lib.elem hostname installOn) {
   home = {
     file = {
       # Copy the file to make it writeable.
-      "${config.xdg.dataHome}/lutris/system_source.yml" = {
+      "${config.xdg.configHome}/lutris/system_source.yml" = {
         source = packages.lutris-config + "/etc/lutris/system.yml";
-        onChange = ''cat ${config.xdg.dataHome}/lutris/system_source.yml > ${config.xdg.dataHome}/lutris/system.yml'';
+        onChange = ''cat ${config.xdg.configHome}/lutris/system_source.yml > ${config.xdg.configHome}/lutris/system.yml'';
       };
     };
     # packages = with pkgs; [
