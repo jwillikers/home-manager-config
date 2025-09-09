86e8c2e36bbed663223e2f0ee2f2c3ea3e6bccd0
Jordan Williams <jordan@jwillikers.com>
2025-09-09 08:27:41 -0500

    Add protonup-ng

---
+++ _mixins/features/gaming/protonup-ng/default.nix
@@ -0,0 +1,20 @@
+{
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  installOn = [
+    "precision5350"
+    "steamdeck"
+    "x1-yoga"
+  ];
+in
+lib.mkIf (lib.elem hostname installOn) {
+  home = {
+    packages = with pkgs; [
+      protonup-ng
+    ];
+  };
+}
