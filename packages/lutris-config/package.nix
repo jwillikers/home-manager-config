ce7220ac0c0a67c510433976c6b23cd776a22b1c
Jordan Williams <jordan@jwillikers.com>
2025-09-10 10:44:32 -0500

    Update Lutris Gamescope options

--- packages/lutris-config/package.nix
+++ packages/lutris-config/package.nix
@@ -5,14 +5,14 @@
 
 stdenvNoCC.mkDerivation {
   pname = "lutris-config";
-  version = "0-unstable-2025-09-05";
+  version = "0-unstable-2025-09-10";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "jwillikers";
     repo = "lutris-config";
-    rev = "026fe911fb5ee4651f489e08bb76c2bdf7015063";
-    hash = "sha256-a7uRMFBGfgRRLmyWysavQffImADcfp9h3r6sAkTg6vQ=";
+    rev = "e34982926f7bf836cdc80937a7caf558a9037b62";
+    hash = "sha256-RUb0DlpWAlirlpzRuVAROM+439A0CsX1On8+Ol+0ILs=";
   };
 
   installPhase = ''
