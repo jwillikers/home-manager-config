e4210b79a64a9268b9610d231f0d243bdbdecc70
Jordan Williams <jordan@jwillikers.com>
2025-09-10 10:16:18 -0500

    Fix Ludusavi config

--- packages/ludusavi-config/package.nix
+++ packages/ludusavi-config/package.nix
@@ -5,14 +5,14 @@
 
 stdenvNoCC.mkDerivation {
   pname = "ludusavi-config";
-  version = "0-unstable-2025-09-09";
+  version = "0-unstable-2025-09-10";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "jwillikers";
     repo = "ludusavi-config";
-    rev = "8970afc65f9633875a68c30249b14812b124cab1";
-    hash = "sha256-qkp7EQhAY9J4Xy5KWFUHF2AXOwSAG8EtPHoii824pAc=";
+    rev = "09f39f793e0ae563d74c2068b4d03539cb77b984";
+    hash = "sha256-Ck0i4mpMkVI8WvrP7kfyi0IJ0vaOFkuBujUfGajUw9k=";
   };
 
   installPhase = ''
--- packages/ludusavi-steam-deck-config/package.nix
+++ packages/ludusavi-steam-deck-config/package.nix
@@ -5,14 +5,14 @@
 
 stdenvNoCC.mkDerivation {
   pname = "ludusavi-steam-deck-config";
-  version = "0-unstable-2025-09-09";
+  version = "0-unstable-2025-09-10";
 
   src = fetchFromGitea {
     domain = "codeberg.org";
     owner = "jwillikers";
     repo = "ludusavi-steam-deck-config";
-    rev = "48547f5b799eca5ed890bba9344814bbec5e1d52";
-    hash = "sha256-FLIxOmq1QiT6XG6lYaCjx2EXelRD03EeAKWi/QHmYRo=";
+    rev = "3a31399c768a1bd083e82ba66fcdc1a669e88d64";
+    hash = "sha256-1e45CjqVcmg1w00gh5eADy0e3ZbcdLO9aQn7yiYSFiY=";
   };
 
   installPhase = ''
