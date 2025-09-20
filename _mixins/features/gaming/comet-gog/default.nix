b899cad0bc616ac5f09f8fa052f7bd2c6702c3f2
Jordan Williams <jordan@jwillikers.com>
2025-09-20 14:00:01 -0500

    Add Comet GOG

---
+++ _mixins/features/gaming/comet-gog/default.nix
@@ -0,0 +1,18 @@
+{
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  installOn = [
+    "precision5350"
+    "steamdeck"
+    "x1-yoga"
+  ];
+in
+lib.mkIf (lib.elem hostname installOn) {
+  home.packages = with pkgs; [
+    comet-gog
+  ];
+}
