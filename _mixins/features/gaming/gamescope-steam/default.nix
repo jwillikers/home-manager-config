7d2d63b53424b4ca9dd64a51412dba5560827b45
Jordan Williams <jordan@jwillikers.com>
2025-12-22 10:21:08 -0600

    Install a shortcut for starting Steam in big picture mode in gamescope
    Add additional utility script for exiting Steam in big picture mode.

---
+++ _mixins/features/gaming/gamescope-steam/default.nix
@@ -0,0 +1,41 @@
+# Provide a desktop shortcut to launch Steam in Big Picture Mode through gamescope.
+#
+# This makes it easy to launch Steam from desktop mode on the Steam Deck and have Steam run as if in gaming mode.
+# This allows my break timer to work while gaming in Steam Big Picture Mode.
+# Be sure to completely exit Steam before running this shortcut, otherwise nothing will happen.
+#
+# A script is included to exit Steam from Big Picture Mode.
+# The script is installed to ~/.config/gamescope-steam/exit-steam-big-picture-mode.sh.
+# Right-click this script and select "Add to Steam" to add it as a shortcut in Steam.
+# Run the shortcut from within Steam to exit Big Picture Mode.
+{
+  config,
+  hostname,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  installOn = [
+    # "precision5350"
+    "steamdeck"
+    # "x1-yoga"
+  ];
+in
+lib.mkIf (lib.elem hostname installOn) {
+  home = {
+    file = {
+      "${config.xdg.dataHome}/applications/steam-big-picture-mode.desktop" =
+        ./steam-big-picture-mode.desktop;
+      # Right-click this script and select "Add to Steam" to add it as a shortcut in Steam.
+      # The script is installed to ~/.config/gamescope-steam/exit-steam-big-picture-mode.sh.
+      "${config.xdg.configHome}/gamescope-steam/exit-steam-big-picture-mode.sh" = {
+        source = ./exit-steam-big-picture-mode.sh;
+        executable = true;
+      };
+    };
+    packages = with pkgs; [
+      steamtinkerlaunch
+    ];
+  };
+}
---
+++ _mixins/features/gaming/gamescope-steam/exit-steam-big-picture-mode.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Exit Steam Big Picture Mode and shutdown Steam.
+# Right-click this script and select "Add to Steam" to add it as a shortcut in Steam.
+# From within Steam Big Picture Mode, select this shortcut to exit Big Picture Mode and shutdown Steam.
+steam -shutdown
