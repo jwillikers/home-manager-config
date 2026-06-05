9765294d507a8267c742169fe44042be0aa63aae
Jordan Williams <jordan@jwillikers.com>
2026-06-05 10:52:31 -0500

    Add some more font packages to help pdfsizeopt

--- _mixins/features/fonts/default.nix
+++ _mixins/features/fonts/default.nix
@@ -15,6 +15,29 @@
       fira
       font-awesome
       liberation_ttf
+
+      # font-ibm-type1
+      # caladea
+      # symbola
+
+      vista-fonts
+      garamond-libre
+      libre-caslon
+      # input-fonts # License
+      libre-franklin
+
+      # tex-gyre.heros
+      # tex-gyre-math.termes
+      # tex-gyre.termes
+      # texlivePackages.heros-otf
+      # texlivePackages.termes-otf
+
+      # texlivePackages.palatino
+      # texlivePackages.zapfding
+      # texlivePackages.fontawesome
+      # texlivePackages.fontawesome5
+
+      noto-fonts
       noto-fonts-color-emoji
       noto-fonts-monochrome-emoji
       source-serif
