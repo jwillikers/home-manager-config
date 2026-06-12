3842b514bafb2c032f4a164919c3ddcd4f6f3e08
Jordan Williams <jordan@jwillikers.com>
2026-06-12 09:00:26 -0500

    Update nvidia drivers version

--- _mixins/hardware/precision5350/gpu/default.nix
+++ _mixins/hardware/precision5350/gpu/default.nix
@@ -1,10 +1,11 @@
-_: {
+_:
+{
   # NVIDIA requires using the --impure flag.
   targets.genericLinux = {
     gpu.nvidia = {
       enable = true;
-      version = "580.159.03";
-      sha256 = "sha256-MshdmbD2QMlQH2GzndrSCP0CiNAVxPvF/QQ1wHeD+nc=";
+      version = "580.159.04";
+      sha256 = "sha256-weZnYbCI0Xs632y2l53przi+JoTRArABoXbc+vq9yh4=";
     };
     nixGL.prime = {
       card = "01:00.0"; # NVIDIA Card
