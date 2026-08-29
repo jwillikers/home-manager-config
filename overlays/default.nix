03a1ccc3eb0aadb985b9aedf554ecd4f28dc4f3f
Jordan Williams <jordan@jwillikers.com>
2026-08-29 10:43:13 -0500

    Update to the latest version of Stretchly

--- overlays/default.nix
+++ overlays/default.nix
@@ -28,10 +28,15 @@
       ];
     };
   };
+  # Update to the latest version of Stretchly.
+  # https://github.com/NixOS/nixpkgs/pull/474659
   stretchly = _final: prev: {
-    stretchly = prev.stretchly.overrideAttrs (_prevAttrs: {
+    stretchly = prev.stretchly.overrideAttrs (_prevAttrs: rec {
       version = "1.22.1";
-      hash = "sha256-Q5Z6os50E5D5aqBK/k5GBlgo3i1HmpOl3atrhvvPGnI=";
+      src = prev.fetchurl {
+        url = "https://github.com/hovancik/stretchly/releases/download/v${version}/stretchly-${version}.tar.xz";
+        hash = "sha256-Q5Z6os50E5D5aqBK/k5GBlgo3i1HmpOl3atrhvvPGnI=";
+      };
     });
   };
   packages = _final: prev: {
